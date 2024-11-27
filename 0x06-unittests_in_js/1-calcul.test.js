bbb4b2151d26ecdf7eeb698f97e528c066a91b0a
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-11-27 23:39:32 +0200

    1 task

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  it('should return sum of two rounded numbers when type is SUM', () => {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+
+  it('should return subtraction when type is SUBTRACT', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+
+  it('should return division when type is DIVIDE', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+
+  it('should throw error for invalid operation type', () => {
+    assert.throws(() => calculateNumber('INVALID', 1, 2), Error);
+  });
+});
