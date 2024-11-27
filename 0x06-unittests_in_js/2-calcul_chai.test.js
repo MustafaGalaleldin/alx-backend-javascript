e0469cf4c8233d324febf02c9c9cf6d535cb2c75
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-11-27 23:42:20 +0200

    Basic Test Using Chai Assertion Library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+  it('should return sum of two rounded numbers when type is SUM', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+
+  it('should return subtraction when type is SUBTRACT', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+
+  it('should return division when type is DIVIDE', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+
+  it('should throw error for invalid operation type', () => {
+    expect(() => calculateNumber('INVALID', 1, 2)).to.throw(Error);
+  });
+});
